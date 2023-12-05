6bad096df1aff1c8316004bf3155c0eb3c77d37c
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-12-05 17:40:40 -0500

    Change text on 404 page

--- src/routes/notFound.jsx
+++ src/routes/notFound.jsx
@@ -10,10 +10,15 @@ function NotFound() {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)] bg-light-navbar dark:bg-dark-background"> {/* Adjust the min-h value based on the height of your navigation bar */}
-            <h1 className="text-2xl mb-1 font-bold text-light-primary-text dark:text-dark-primary-text">404 Not Found</h1>
-            <p className="mb-4 text-light-secondary-text dark:text-dark-secondary-text">The page you are looking for has been <em>forgotten.</em></p>
-            <button onClick={goHome} className="px-4 py-2 rounded-md text-light-primary-text dark:text-dark-primary-text bg-light-accent dark:bg-dark-accent hover:bg-light-sidebar dark:hover:bg-dark-sidebar focus:outline-none focus:ring-2 focus:ring-inset focus:ring-dark-accent">
-                Return Home
+            <h1 className="text-3xl mb-2 font-bold text-light-primary-text dark:text-dark-primary-text">404 Page Not Found</h1>
+            <p className="mb-2 text-lg text-light-secondary-text dark:text-dark-secondary-text">
+                You've wandered into an uncharted fragment of Immemoria. Perhaps this memory has yet to be written, or maybe it has been forgotten.
+            </p>
+            <p className="mb-4 text-lg text-light-secondary-text dark:text-dark-secondary-text">
+                Fear not, for not all who wander are lost.
+            </p>
+            <button onClick={goHome} className="px-5 py-3 rounded-md text-lg text-light-primary-text dark:text-dark-primary-text bg-light-accent dark:bg-dark-accent hover:bg-light-sidebar dark:hover:bg-dark-sidebar focus:outline-none focus:ring-2 focus:ring-inset focus:ring-dark-accent">
+                Return to the known
             </button>
         </div>
     );
