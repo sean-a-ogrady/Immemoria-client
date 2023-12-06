1f98d575457c4e9d1f795fab5943e4d58118b362
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-12-06 17:36:16 -0500

    Add collapsing and animations

--- src/routes/gameInterface.jsx
+++ src/routes/gameInterface.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Disclosure } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon, ChevronUpIcon, ChevronDownIcon, CogIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon, XMarkIcon, ChevronLeftIcon, ChevronRightIcon, ChevronUpIcon, ChevronDownIcon, CogIcon } from '@heroicons/react/24/outline';
 
 export default function GameInterface() {
-    
+
+    const [isDesktopSidebarOpen, setIsDesktopSidebarOpen] = useState(true);
+
     const handleSettingsClick = () => {
         // Implement logic to open settings window
     };
+
+    const toggleSidebar = () => { setIsDesktopSidebarOpen(!isDesktopSidebarOpen); };
+
+    const sidebarWidth = isDesktopSidebarOpen ? '320px' : '0px';
 
     return (
         <div className="flex flex-row min-h-[calc(100vh-72px)] max-h-[calc(100vh-72px)] bg-light-background dark:bg-dark-background">
@@ -23,7 +29,12 @@ export default function GameInterface() {
                         </div>
 
                         {/* Combined Sidebar Content for Mobile and Desktop */}
-                        <div className={`${open ? 'block' : 'hidden'} h-full sm:block px-4 py-2 w-[100vw] sm:w-[320px] bg-light-sidebar dark:bg-dark-sidebar z-10 flex flex-col overflow-y-auto`}>
+                        <div
+                            className={`${open ? 'block' : 'hidden'} h-full sm:block px-4 py-2
+                                ${isDesktopSidebarOpen ? 'sm:w-[320px] sm:px-4 sm:py-2' : 'sm:w-0 sm:px-0 sm:py-0'} 
+                                w-[100vw] bg-light-sidebar dark:bg-dark-sidebar z-10 flex flex-col overflow-y-auto`}
+                            style={{ transition: 'width 0.3s' }}
+                        >
                             {/* Section header - Memories */}
                             <h2 className="py-2 text-m font-medium text-light-primary-text dark:text-dark-primary-text">Memories</h2>
 
@@ -44,7 +55,7 @@ export default function GameInterface() {
                             {/* Settings */}
                             <button
                                 onClick={handleSettingsClick}
-                                className="flex items-center w-full mb-3 text-left font-semibold text-light-primary-text dark:text-dark-primary-text hover:text-light-accent dark:hover:text-dark-accent"
+                                className="flex items-center w-full mb-3 text-left font-semibold text-light-primary-text dark:text-dark-primary-text hover:text-light-accent dark:hover:text-dark-accent transition-colors duration-200"
                             >
                                 <CogIcon className="h-5 w-5 mr-2" />
                                 Settings
@@ -53,6 +64,16 @@ export default function GameInterface() {
                     </>
                 )}
             </Disclosure>
+
+            {/* Sidebar Toggle Button for Desktop */}
+            <div className="hidden sm:flex sm:absolute sm:left-[320px] sm:top-1/2 sm:-translate-y-1/2 sm:translate-x-1 z-20" style={{ left: isDesktopSidebarOpen ? '320px' : '0px', transition: 'left 0.3s' }}>
+                <button
+                    onClick={toggleSidebar}
+                    className="text-light-primary-text dark:text-dark-primary-text hover:text-light-accent dark:hover:text-dark-accent transition-colors duration-200"
+                >
+                    {isDesktopSidebarOpen ? <ChevronLeftIcon className="h-5 w-5" /> : <ChevronRightIcon className="h-5 w-5" />}
+                </button>
+            </div>
 
             {/* Center Content */}
             <div className="flex-grow flex justify-center">
@@ -66,7 +87,7 @@ const SidebarSection = ({ title }) => (
     <Disclosure>
         {({ open }) => (
             <>
-                <Disclosure.Button className="flex justify-between w-full py-2 text-sm font-medium text-left text-light-primary-text dark:text-dark-primary-text hover:text-light-accent dark:hover:text-dark-accent">
+                <Disclosure.Button className="flex justify-between w-full py-2 text-sm font-medium text-left text-light-primary-text dark:text-dark-primary-text hover:text-light-accent dark:hover:text-dark-accent transition-colors duration-200">
                     {title}
                     <span className="ml-2">
                         {open ? <ChevronUpIcon className="h-5 w-5" /> : <ChevronDownIcon className="h-5 w-5" />}
