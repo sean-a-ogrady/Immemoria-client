fe2decacedbf82a74155d7b14683d9f29623e2b2
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-12-21 10:20:10 -0500

    Add background images and implement image slideshow on landing page

--- src/routes/landingPage.jsx
+++ src/routes/landingPage.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { url } from '../main';
+import cyberpunk from '../assets/background/cyberpunk.png';
+import egypt from '../assets/background/egypt.png';
+import fantasy from '../assets/background/fantasy.png';
+import forest from '../assets/background/forest.png';
+import greece from '../assets/background/greece.png';
+import knight from '../assets/background/knight.png';
+import love from '../assets/background/love.png';
+import primal from '../assets/background/primal.png';
+import space_battle from '../assets/background/space_battle.png';
+import urban_fantasy from '../assets/background/urban_fantasy.png';
+import victorian from '../assets/background/victorian.png';
 
 function LandingPage() {
     const navigate = useNavigate();
+    // index of the current image being displayed
+    const [currentImage, setCurrentImage] = useState(0);
+    const [fadeIn, setFadeIn] = useState(true);
+
+    const images = [
+        cyberpunk, egypt, fantasy, forest, greece,
+        knight, love, primal, space_battle, urban_fantasy, victorian
+    ];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setFadeIn(false);
+          setTimeout(() => {
+            setCurrentImage((currentImage + 1) % images.length);
+            setFadeIn(true);
+          }, 2000); // Fade out duration
+        }, 4000); // Total duration for each image
+    
+        return () => clearInterval(interval);
+      }, [currentImage])
 
     const startGame = () => {
         navigate('/play');
@@ -28,13 +59,21 @@ function LandingPage() {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-[calc(100vh-72px)] bg-light-background dark:bg-dark-background text-center px-4">
-          {/* ^^^ Adjust min-h based on the height of navbar  - Currently set to 72px */}
-            <div className="flex flex-col items-center justify-center space-y-6">
+        <div className="relative flex flex-col items-center justify-center min-h-[calc(100vh-72px)] text-center px-4">
+            {/* ^^^ Adjust min-h based on the height of navbar  - Currently set to 72px */}
+            {/* Background color div */}
+            <div className="absolute top-0 left-0 w-full h-full bg-light-background dark:bg-dark-background -z-20"></div>
+            {/* background image */}
+            <img 
+                src={images[currentImage]} 
+                alt="Immemoria background" 
+                className={`-z-10 absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-2000 ${fadeIn ? 'opacity-100' : 'opacity-0'}`}
+            />
+            <div className="relative z-10 flex flex-col items-center justify-center space-y-6 bg-white dark:bg-black dark:bg-opacity-50 bg-opacity-50 p-6 rounded-md">
                 <h1 className="text-4xl font-bold text-light-primary-text dark:text-dark-primary-text">
                     Welcome to Immemoria
                 </h1>
-                <p className="text-lg text-light-secondary-text dark:text-dark-secondary-text">
+                <p className="text-lg text-light-primary-text dark:text-dark-primary-text">
                     Embark on a journey through a world where memories shape reality. Explore forgotten lands, uncover lost stories, and reshape the fabric of existence.
                 </p>
                 <button 
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,11 +28,18 @@ export default {
       },
       animation: {
         'fade-in': 'fadeIn 0.5s ease-out',
+        'fade-in-slow': 'fadeIn 2s ease-out',
+        'fade-out': 'fadeOut 0.5s ease-out',
+        'fade-out-slow': 'fadeOut 2s ease-out',
       },
       keyframes: {
         fadeIn: {
           '0%': { opacity: '0' },
           '100%': { opacity: '1' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
         },
       }
     },
